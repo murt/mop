0afe0bbd7babdd3f361d5f746ae8f9b25c8fd56a
murt <murt@unpossible.io>
2017-04-20 11:06:44 +1000

    #1 - Targetting commonjs so imports are correctly rendered. Using the bulma css framework as well as sass styles now. Including a font-face and some layout in the pug template before switching it to react. createWindow is now parameterised before moving over to a config system. Added gameplay font.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const gclean = require(`gulp-clean`);
 const gts = require(`gulp-typescript`);
 const gtd = require(`gulp-typedoc`);
 const gpug = require(`gulp-pug`);
+const gconcat = require(`gulp-concat`);
+const gsass = require(`gulp-sass`);
+const gsrc = require(`gulp-add-src`);
 
 const pkg = require(`./package.json`);
 
@@ -12,6 +15,9 @@ gulp.task(`build#scripts`, () => {
     const tsp = gts.createProject(`tsconfig.json`);
     return tsp.src()
     .pipe(tsp())
+    .once("error", function () {
+        this.once("finish", () => process.exit(1));
+    })
     .js.pipe(gulp.dest(`target/dist`));
 });
 
@@ -25,16 +31,28 @@ gulp.task(`build#html`, () => {
 
 /**
 */
-/*
-gulp.task(`build#electron`, () => {
-    console.log(`building electron...`);
+gulp.task(`build#styles`, () => {
+    return gulp.src([`src/main/sass/style.sass`])
+    .pipe(gsass().on(`error`, gsass.logError))
+    .pipe(gsrc.prepend(`node_modules/bulma/css/bulma.css`))
+    .pipe(gconcat(`style.css`))
+    .pipe(gulp.dest(`target/dist`));
 });
-*/
 
 /**
 */
-gulp.task(`build`, [`scripts`, `html`].map(t => `build#${t}`), () => {
+gulp.task(`build#fonts`, () => {
+    return gulp.src([`src/main/font/*.*`])
+    .pipe(gulp.dest(`target/dist/fonts`));
 });
+
+/**
+*/
+gulp.task(
+    `build`,
+    [`scripts`, `html`, `styles`, `fonts`].map(t => `build#${t}`),
+    () => {}
+);
 
 /**
 */
--- src/main/ts/main.ts
+++ src/main/ts/main.ts
@@ -3,10 +3,13 @@ import {app, BrowserWindow} from 'electron';
 const path = require('path');
 const url = require('url');
 
+//
 let win:Electron.BrowserWindow;
 
-function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600});
+/**
+ */
+function createWindow ({width=800, height=600}:{width?:number, height?:number}={}) {
+  win = new BrowserWindow({width, height});
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -21,14 +24,17 @@ function createWindow () {
   });
 }
 
+//
 app.on('ready', createWindow);
 
+//
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+//
 app.on('activate', () => {
   if (win === null) {
     createWindow();
