b28a5295c8c277e8db53a6a848e88a2f9a2a3f52
murt <murt@unpossible.io>
2017-04-24 10:12:27 +1000

    #1 - Working on generating the html report has a tentative solution in place for the coverage by manually generating it with a gulp task after all the remapping has finished.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const path = require(`path`);
 const gulp = require(`gulp`);
 const gclean = require(`gulp-clean`);
 const gts = require(`gulp-typescript`);
@@ -9,6 +10,7 @@ const gsass = require(`gulp-sass`);
 const gsrc = require(`gulp-add-src`);
 const gmocha = require(`gulp-mocha`);
 const gistanbul = require(`gulp-istanbul`);
+const greport = require(`gulp-istanbul-report`);
 const gremap = require(`remap-istanbul/lib/gulpRemapIstanbul`);
 const runSequence = require(`run-sequence`);
 const isparta = require(`isparta`);
@@ -18,7 +20,8 @@ const fsx = require(`fs-extra`);
 const pkg = require(`./package.json`);
 
 /**
-*/
+ * Compile TypeScript.
+ */
 gulp.task(`build:scripts`, () => {
     const tsp = gts.createProject(`tsconfig.json`);
     return tsp.src()
@@ -32,7 +35,8 @@ gulp.task(`build:scripts`, () => {
 });
 
 /**
-*/
+ * Compile Pug templates.
+ */
 gulp.task(`build:html`, () => {
     return gulp.src([`src/main/pug/*.pug`])
     .pipe(gpug({}))
@@ -40,7 +44,8 @@ gulp.task(`build:html`, () => {
 });
 
 /**
-*/
+ * Compile SASS templates and concatenate any third-party frameworks.
+ */
 gulp.task(`build:styles`, () => {
     return gulp.src([`src/main/sass/style.sass`])
     .pipe(gsass().on(`error`, gsass.logError))
@@ -50,7 +55,8 @@ gulp.task(`build:styles`, () => {
 });
 
 /**
-*/
+ * Copy fonts over.
+ */
 gulp.task(`build:fonts`, () => {
     return gulp.src([`src/main/font/*.*`])
     .pipe(gulp.dest(`target/dist/fonts`));
@@ -65,7 +71,9 @@ gulp.task(
 );
 
 /**
-*/
+ * Compile the specs - this needs to be done as the specs are written
+ * in TypeScript.
+ */
 gulp.task(`test:specs`, () => {
     return gulp.src([`src/test/specs/*.ts`])
     .pipe(gts())
@@ -73,32 +81,37 @@ gulp.task(`test:specs`, () => {
 });
 
 /**
-*/
+ * Run the tests on the compiled specs
+ */
 gulp.task(`test`, [`build`, `test:specs`], () => {
     return gulp.src([`target/test/specs/*.spec.js`], {read:false})
     .pipe(gmocha());
 });
 
 /**
-*/
+ * Instrument the compiled
+ */
 gulp.task(`cover:instrument`, () => {
     return gulp.src([`target/dist/*.js`])
+    .pipe(gsourcemaps.init())
     .pipe(gistanbul({
         includeUntested: true,
         coverageVariable: `__coverage__`
     }))
+    .pipe(gsourcemaps.write())
     .pipe(gulp.dest(`target/test/dist`))
 });
 
 /**
 */
-gulp.task(`cover:test`, () => {
+gulp.task(`cover:test`, [`test:specs`], () => {
     return gulp.src([`target/test/specs/*.spec.js`], {read:false})
     .pipe(gmocha());
 });
 
 /**
-*/
+ * Combine coverage reports for all tests that have been run.
+ */
 gulp.task(`cover:combine`, () => {
     combine.sync({
         dir: `target/reports/coverage`,
@@ -111,26 +124,49 @@ gulp.task(`cover:combine`, () => {
 });
 
 /**
-*/
+ * Remap the coverage reports back to their original source files and generate
+ * reports based on that.
+ */
 gulp.task(`cover:remap`, () => {
     return gulp.src([`target/reports/coverage/coverage-combined.json`])
     .pipe(gremap({
-        basePath: 'src/main/ts',
+        basePath: path.resolve(`src`, `main`, `ts`),
+        fail: true,
         reports: {
-            'text': '',
+            text: '',
             json: 'target/reports/coverage/coverage-remap.json',
-            'html': 'target/reports/coverage/html',
             lcovonly: 'target/reports/coverage/lcov.info'
         }
     }));
 });
 
+/**
+ * Generate the html report manually in a task - this is primarily done
+ * as the remap-istanbul task fails to generate the html report
+ * correctly. Note that this is a relatively tentative solution
+ */
+gulp.task(`cover:html`, () => {
+    return gulp.src([`target/reports/coverage/coverage-remap.json`])
+    .pipe(greport({
+        reporters: [{
+            name: `html`,
+            dir: `target/reports/coverage/html`
+        }]
+    }));
+});
+
+/**
+ * Manually create certain target directories as some of the gulp tasks
+ * will not work unless they already exist.
+ */
 gulp.task('targets', () => {
     fsx.mkdirsSync('target/reports/coverage');
 });
 
+/**
+*/
 gulp.task(`cover`, function (done) {
-    runSequence(`targets`, `test:specs`, `cover:instrument`, `cover:test`, `cover:combine`, `cover:remap`, done);
+    runSequence(`targets`, `cover:instrument`, `cover:test`, `cover:combine`, `cover:remap`, `cover:html`, done);
 });
 
 /**
