78410274b32e6ac451394e05bff2632b4909e864
murt <murt@unpossible.io>
2017-04-27 09:41:08 +1000

    #5 - The App component is now the main entry point and renders to the page

--- src/main/ts/app.tsx
+++ src/main/ts/app.tsx
@@ -2,11 +2,22 @@ import * as React from 'react';
 
 interface AppProps {}
 
-export default class App extends React.Component<AppProps, {}> {
+export class App extends React.Component<AppProps, {}> {
 
   render () {
     return (
-      <span>Application Code</span>
+      <div>
+        <nav className="nav has-shadow">
+          <div className="nav-left">
+            <a id="mop-logo" className="nav-item">MOP</a>
+          </div>
+        </nav>
+        <div className="container is-fluid">
+          <div className="notification">
+            This is the main mop container created by react
+          </div>
+        </div>
+      </div>
     );
   }
 
